e25d908fa2143d0594ca34af47328331028a44f6
Stijn Kuipers <stijnkuipers@gmail.com>
2015-12-12 19:38:25 +0100

    font back to font2, connection class setup

--- examples/flowgraph/index.js
+++ examples/flowgraph/index.js
@@ -1,6 +1,9 @@
 //Pure JS based composition
-define.class(function($server$,composition, role, $ui$,treeview,  cadgrid, splitcontainer, screen, view, label, button, $widgets$, propviewer, colorpicker){	
+define.class(function($server$,composition, role,require, $ui$,treeview,  cadgrid, splitcontainer, screen, view, label, button, $widgets$, propviewer, colorpicker){	
 	
+	var Shader = this.Shader = require('$system/platform/$platform/shader$platform')
+
+		
 	define.class(this, "menubar", function($ui$, view){
 		
 	})
@@ -20,24 +23,59 @@ define.class(function($server$,composition, role, $ui$,treeview,  cadgrid, split
 			return [
 				view({bgcolor:"#202220", margin:vec4(0,0,0,0), padding:vec4(0)},
 					view({margin:vec4(1,1,2,0),bgcolor:"#8080a0", borderwidth:0,borderradius:vec4(4,4,0,0),padding:vec4(10,2,10,2)},
-						label({text:this.title, margin:1, bg:0, fontsize:14, fgcolor: "white" })
+						label({text:this.title, margin:1, bg:0, fontsize:12, fgcolor: "white" })
 					)
 				)
 				,this.constructor_children
 			];
 		}
 	})
-	define.class(this, "connection", function($ui$, view){
-		
-		this.bg = {
-			
+	
+	define.class(this, "connection",function($ui$, view){	
+		this.attributes = {
+			from:{type:String, value:""},
+			to:{type:String, value:""},
+			frompos:{type:vec2, value:vec2(0,0)},
+			topos:{type:vec2, value:vec2(100,100)},
+			linewidth:{type:float, value:14}
 		}
-	}) 
+		
+		this.bgcolor = "blue" 
+		
+		define.class(this, "connectionshader", this.Shader,function($ui$, view){	
+			this.updateorder = 0
+			this.draworder = 0
+			this.mesh = vec2.array()
+			
+			for(var i = 0;i<100;i++){
+				this.mesh.push([i/100,-0.5])
+				this.mesh.push([i/100, 0.5])
+			}
+			
+			
+			this.position = function(){
+				uv = mesh.xy
+				var a = mesh.x;
+				var b = mesh.y * view.linewidth/2;
+				pos = view.frompos * a + view.topos * (1-a);
+				//pos = vec2(mesh.x * view.layout.width, mesh.y * view.layout.height)
+				return vec4(pos, 0, 1) * view.totalmatrix * view.viewmatrix
+			}
+			
+			this.color = function(){
+				return view.bgcolor
+			}	
+		}) 
+		
+		this.bg = this.connectionshader();
+	})
+	
 	define.class(this, "block", function($ui$, view, label){
+		
 		
 		this.position = "absolute" ;
 		this.bgcolor = vec4("#708090" )
-		this.padding = 5;
+		this.padding = 2;
 		this.borderradius = vec4(10,10,0,0);
 		this.borderwidth = 0;
 		
@@ -84,7 +122,7 @@ define.class(function($server$,composition, role, $ui$,treeview,  cadgrid, split
 
 		this.render = function(){
 			return [
-				label({text:this.title, bg:0})
+				label({text:this.title, bg:0, margin:4, fontsize: 12})
 				,view({flexdirection:"row", alignitems:"stretch", bg:0 }
 					,button({icon:"flask", text:"button" })
 					,button({icon:"flask"})
@@ -108,7 +146,7 @@ define.class(function($server$,composition, role, $ui$,treeview,  cadgrid, split
 					,this.dockpanel({title:"Patch"}
 						,cadgrid({name:"centralconstructiongrid", overflow:"scroll" ,bgcolor: "#303030",minorsize:5,majorsize:25,  majorline:"#505040", minorline:"#404040"}
 							,view({name:"connectionlayer", bg:0}
-								,this.connection({from:"phone", to:"tv"})
+								,this.connection({from:"phone", to:"tv", bgcolor:"blue" })
 							)
 							,view({name:"blocklayer", bg:0}
 								,this.block({name:"phone", title:"Phone", x:200, y:20})
