da415e166eded54a3e20db0861fde2de93f5ec59
Phil <philip@pbrdev.com>
2015-12-28 09:08:25 -0800

    Changed directory names to make it clear the tests are intended for dali.

--- examples/dalitests/images.js
+++ examples/dalitests/images.js
@@ -8,7 +8,7 @@ define.class(function($server$, composition, $ui$, screen, view, label, require)
 			for (var w=100; w<=200; w+= 25) {
 				var v1 = view({
 					size: vec2(w, w)
-					,bgimage: require('./images/' + digit + '.png')
+					,bgimage: require('./assets/' + digit + '.png')
 				})
 				dynviews.push(v1);
 			}
