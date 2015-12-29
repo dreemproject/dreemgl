c507cd0922a3e3739ad25fe490c1d430206214b1
Stijn Kuipers <stijnkuipers@gmail.com>
2015-12-29 17:04:01 +0100

    Menu progress

--- examples/menutest.js
+++ examples/menutest.js
@@ -3,17 +3,21 @@ define.class('$server/composition', function($ui$, screen, view, button,label, s
 	
 	define.class(this, "popupmenu", function($ui$, view, label, button){
 		this.bgcolor = "gray" 
-		this.flexdirection = "column" 
+		this.flexdirection = "row" 
 		this.attributes = {
-				menus:[{
-					name:"File", commands:[
-				
-					{name:"New project with a long name", action:function(){console.log("new project with a long name") ;}},
-					{name:"Open", action:function(){console.log("Open") ;}},
-					{name:"Save", action:function(){console.log("Save") ;}}
-				]
-				}
-					
+				menus:[
+					{name:"File", commands:[				
+							{name:"New project with a long name", action:function(){console.log("new project with a long name") ;}},
+							{name:"Open", action:function(){console.log("Open") ;}},
+							{name:"Save", action:function(){console.log("Save") ;}}
+						]
+					}
+					,{name:"Edit", commands:[				
+							{name:"New project with a long name", action:function(){console.log("new project with a long name") ;}},
+							{name:"Open", action:function(){console.log("Open") ;}},
+							{name:"Save", action:function(){console.log("Save") ;}}
+						]
+					}
 			]}
 			
 			
@@ -25,22 +29,22 @@ define.class('$server/composition', function($ui$, screen, view, button,label, s
 			for(var m in this.menus){
 				var res = [];
 				var menu = this.menus[m];
-							
-				mres.push(button({ text:m.name, menucommands: menu, click: function()
+				console.log("menu:" , menu);
+				mres.push(button({ text:menu.name, menucommands: menu.commands, click: function()
 				{
 					// lets open a modal dialog
 					var res = [];
-					console.log(this.menucommands);
+					console.log("full menucommands array", this.menucommands);
 					for(var a in this.menucommands){
 						var c = this.menucommands[a];
-						console.log(c);
+						console.log("menucommand: ", c);
 						res.push(button({text:c.name,clickaction: c.action, click:function(){if(this.clickaction) this.clickaction(); this.closeModal(true);}, margin:1}))
 					}
 					
 					this.screen.openModal(
 						view({bg:false,flexdirection:"column",
 							miss:function(){
-								this.resolve(false)
+								this.closeModal(false)
 							},
 							init:function(){
 								console.log('here')
@@ -66,7 +70,7 @@ define.class('$server/composition', function($ui$, screen, view, button,label, s
 		screen({clearcolor:vec4('black')},
 			view({flex:1, bg:'false'},
 				speakergrid({ }
-				,view({bgcolor:"green" , flexdirection:"column"},view({bg:false},this.popupmenu({position:"relative",margin:10,flex:0}))))
+				,view({bgcolor:vec4(0,0.1,0.2,0.8) , flexdirection:"column"},view({bg:false},this.popupmenu({position:"relative",margin:10,flex:0}))))
 			)
 		)
 	]}
