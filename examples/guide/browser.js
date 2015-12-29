cbc7381578af1b6cb5a59f96fa67a22dba540527
gnovos <freemason@gmail.com>
2015-12-29 15:46:25 -0800

    Cleanup regression

--- examples/guide/browser.js
+++ examples/guide/browser.js
@@ -1,4 +1,4 @@
-define.class(function($ui$, screen, view, button, label, $$, movie) {
+define.class(function($ui$, screen, view, button, textbox, label, $$, movie) {
 
     this.attributes = {
         term: Config({type:String}),
@@ -21,11 +21,11 @@ define.class(function($ui$, screen, view, button, label, $$, movie) {
         return [
 
         view(
-            {flexdirection:'column', flex:1, overflow:'scroll'},
-            label({ name:'search', width:300, height:30, text:'Aliens', fgcolor:'black'}),
+            { flexdirection:'column', flex:1, overflow:'scroll' },
+            textbox({ name:'search', width:300, height:30, value:'Aliens', fgcolor:'black'}),
             button({text:'Search', width:90, click:function() {
                 // sets the term on our screen, this should fire the server thing
-                this.screen.term = this.parent.search.text;
+                this.screen.term = this.parent.find('search').value;
             }}),
             view(this.renderMovies())
         )
