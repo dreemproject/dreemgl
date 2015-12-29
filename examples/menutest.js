bbb40c813ee83117d30df6d3ca90c0b05f665afb
Stijn Kuipers <stijnkuipers@gmail.com>
2015-12-29 17:18:44 +0100

    Menu..

--- examples/menutest.js
+++ examples/menutest.js
@@ -2,7 +2,7 @@
 define.class('$server/composition', function($ui$, screen, view, button,label, speakergrid){
 	
 	define.class(this, "popupmenu", function($ui$, view, label, button){
-		this.bgcolor = "gray" 
+		this.bgcolor = "#585858" 
 		this.flexdirection = "row" 
 		this.attributes = {
 				menus:[
@@ -32,7 +32,7 @@ define.class('$server/composition', function($ui$, screen, view, button,label, s
 				var res = [];
 				var menu = this.menus[m];
 				console.log("menu:" , menu);
-				mres.push(button({ text:menu.name, menucommands: menu.commands, click: function()
+				mres.push(button({buttoncolor1:"#737373",buttoncolor2:"#737373",hovercolor1:"#3b3b3b",hovercolor2:"#3b3b3b",borderwidth:0, borderradius:4,margin:vec4(4,0,0,0),padding:5,  text:menu.name, menucommands: menu.commands, click: function()
 				{
 					// lets open a modal dialog
 					var res = [];
@@ -40,7 +40,7 @@ define.class('$server/composition', function($ui$, screen, view, button,label, s
 					for(var a in this.menucommands){
 						var c = this.menucommands[a];
 						console.log("menucommand: ", c);
-						res.push(button({text:c.name,buttoncolor1:"#505050",borderwidth:0,hovercolor1:"#606060",hovercolor2:"#606060", buttoncolor2:"#505050",   clickaction: c.action, click:function(){if(this.clickaction) this.clickaction(); this.closeModal(true);}, margin:1}))
+						res.push(button({padding:5,margin:vec4(4),text:c.name,buttoncolor1:"#505050",borderwidth:0,hovercolor1:"#3b3b3b",hovercolor2:"#3b3b3b", buttoncolor2:"#505050",   clickaction: c.action, click:function(){if(this.clickaction) this.clickaction(); this.closeModal(true);}, margin:1}))
 					}
 					
 					this.screen.openModal(
@@ -51,7 +51,7 @@ define.class('$server/composition', function($ui$, screen, view, button,label, s
 							init:function(){
 								console.log('here')
 							},
-							pos:[this.layout.absx,this.layout.absy + this.layout.height+2],
+							pos:[this.layout.absx,this.layout.absy + this.layout.height],
 							size:[300,NaN],position:'absolute'
 						}, res)
 					).then(function(result){
@@ -69,10 +69,10 @@ define.class('$server/composition', function($ui$, screen, view, button,label, s
 	this.alignitems = "center" 
 	
 	this.render = function(){ return [
-		screen({clearcolor:vec4('black')},
+		screen({clearcolor:vec4('black'), title:"Small menu bar test frame"},
 			view({flex:1, bg:'false'},
 				speakergrid({ }
-				,view({bgcolor:vec4(0,0.1,0.2,0.8) , flexdirection:"column"},view({bg:false},this.popupmenu({position:"relative",margin:10,flex:0}))))
+				,view({bgcolor:vec4(0,0.1,0.2,0.8) , flexdirection:"column"},view({bg:false},this.popupmenu({position:"relative",margin:0,flex:1}))))
 			)
 		)
 	]}
