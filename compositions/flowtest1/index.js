2e7556efa269c133fc0cf0ba82679f7f4e6a31ed
Stijn Kuipers <stijnkuipers@gmail.com>
2016-01-07 19:57:24 +0100

    merge

--- compositions/flowtest1/index.js
+++ compositions/flowtest1/index.js
@@ -3,14 +3,16 @@ define.class("$server/composition",function($server$, service, $ui$, screen, vie
 		return [
 			rovi({
 				name:'myservice', 
-				flowdata:{x:235, y:339}, 
+				flowdata:{x:574, y:420}, 
 				query:wire("this.rpc.xy1.mousepos")
 			}),
 			xypad({
 				name:'xy1', 
-				flowdata:{x:20, y:81}
+				flowdata:{x:82, y:93}
 			}),
-			labtext({name:'lab', flowdata:{x:1193, y:136}, number2:wire("this.rpc.myservice.data"), number:wire("this.rpc.xy1.mousepos"), text:wire("this.rpc.xy1.mousepos")})
+			labtext({name:'lab', flowdata:{x:733, y:75}, number2:wire("this.rpc.myservice.data"), number:wire("this.rpc.xy1.mousepos"), text:wire("this.rpc.xy1.mousepos")}),
+			rovi({name:"rovi0", flowdata:{x:135, y:507}, query:wire("this.rpc.xy1.mousepos")}),
+			labtext({name:"labtext0", flowdata:{x:665, y:467}, text:wire("this.rpc.rovi0.data")})
 		]
 	}
 	
