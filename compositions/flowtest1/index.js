fab2a44f61e6844f603ab74d6811c6d6fc5bf83b
Stijn Kuipers <stijnkuipers@gmail.com>
2016-01-07 20:29:43 +0100

    bugging example flowtest

--- compositions/flowtest1/index.js
+++ compositions/flowtest1/index.js
@@ -3,16 +3,16 @@ define.class("$server/composition",function($server$, service, $ui$, screen, vie
 		return [
 			rovi({
 				name:'myservice', 
-				flowdata:{x:574, y:420}, 
+				flowdata:{x:389, y:231}, 
 				query:wire("this.rpc.xy1.mousepos")
 			}),
 			xypad({
 				name:'xy1', 
-				flowdata:{x:82, y:93}
+				flowdata:{x:28, y:116}
 			}),
-			labtext({name:'lab', flowdata:{x:733, y:75}, number2:wire("this.rpc.myservice.data"), number:wire("this.rpc.xy1.mousepos"), text:wire("this.rpc.xy1.mousepos")}),
+			labtext({name:'lab', flowdata:{x:731, y:125}, number2:wire("this.rpc.myservice.data"), number:wire("this.rpc.xy1.mousepos"), text:wire("this.rpc.xy1.mousepos")}),
 			rovi({name:"rovi0", flowdata:{x:135, y:507}, query:wire("this.rpc.xy1.mousepos")}),
-			labtext({name:"labtext0", flowdata:{x:665, y:467}, text:wire("this.rpc.rovi0.data")})
+			labtext({name:"labtext0", flowdata:{x:494, y:478}, text:wire("this.rpc.rovi0.data")})
 		]
 	}
 	
