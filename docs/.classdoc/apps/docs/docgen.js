083f87f189558d7afe4f66df4515926d759e5004
gnovos <freemason@gmail.com>
2016-01-21 11:48:32 -0800

    Remove old code

--- docs/.classdoc/apps/docs/docgen.js
+++
@@ -1,8 +0,0 @@
-/**
- * @class docgen
- * @extends compositionnodejs
- * Special composition that generates JSDuck-style documentation and writes into ./docs/.classdoc
- */
-/**
- * @method render
- */
