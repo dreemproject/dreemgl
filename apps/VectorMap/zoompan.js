4339946254e03b5bf4f0ae65d74bf09e0e64096b
Stijn Kuipers <stijnkuipers@gmail.com>
2016-01-22 02:11:44 +0100

    zoompan stuff

--- apps/VectorMap/zoompan.js
+++ apps/VectorMap/zoompan.js
@@ -1,32 +1,91 @@
-define.class('$server/composition', function vectormap(require,  $server$, fileio,$ui$, numberbox, button, menubar, label, screen, view, foldcontainer, speakergrid,checkbox, icon, $widgets$, colorpicker,  jsviewer, radiogroup, $3d$, ballrotate, $$, urlfetch){
+define.class('$server/composition', function vectormap(require,  $server$, fileio,$ui$, numberbox, button, menubar, label, screen, view, foldcontainer, speakergrid,checkbox, icon, $widgets$, colorpicker,  jsviewer, radiogroup, $3d$, ballrotate, $$, urlfetch, geo){
 	define.class(this, "mapdataset", function($ui$, view)
 	{
 		this.requestPending = false;
 		this.loadqueue = [];
 		this.loadqueuehash = [];
 		this.loadedblocks = {};
+		this.geo = geo();
+		this.attributes = {
+			centerx: 0,
+			centery: 0
+		}
+		
+		this.setCenter = function(x,y,z){
+			this.centerx = x;
+			this.centery = y;
+			this.centerz = z;
 			
+			for(var xx = -3; xx < 3; xx++){
+				for(var yy = -3; yy < 3; yy++){			
+//					// add square around 
+					this.addToQueue(x + xx,y + yy,z);	
+				}
+			}
+			
+		}
 		this.createHash = function(x, y, z){
 			return x + "_" + y + "_" + z;
 		}
 
 		this.addToQueue = function(x, y, z){		
 			var hash = this.createHash(x, y, z);
+			if (this.loadqueuehash[hash]) return; // already queued for load.
+			if (this.loadedblocks[hash]) return; // already loaded.
+			
+//			console.log("adding ",x,y,z);
+			
 			this.loadqueuehash[hash] = 1;
 			this.loadqueue.push({x:x, y:y, z:z});
 			this.updateLoadQueue();
 		}
 
-		this.blockLoaded = function(x, y, z, data){
+		this.processLoadedBlock = function(x, y, z, data){
 			var hash = this.createHash(x,y,z);
 			this.loadedblocks[hash] = {x:x, y:y, z:z, blockdata:data};
 		}
-
+		
+		this.simulateLoaded = function(){
+			if (this.currentRequest) {
+				
+				var r = this.currentRequest;
+				var hash = this.createHash(r.x, r.y, r.z);
+			//	console.log("loaded " , hash);
+				this.loadedblocks[hash] = this.currentRequest
+				this.loadqueuehash[hash] =  undefined;
+				this.currentRequest = undefined;
+				this.cleanLoadedBlocks();
+				this.updateLoadQueue();
+				this.find("tiledmap").datasource = this;
+				
+			}
+		
+		}
+		this.cleanLoadedBlocks = function(){
+			var keys = Object.keys(this.loadedblocks);
+			
+			for(var i =0 ;i<keys.length;i++)
+			{
+				var lb = this.loadedblocks[keys[i]]
+				var dx = lb.x - this.centerx;
+				var dy = lb.y - this.centery;
+				var dz = (lb.z - this.centerz)*5;
+				var dist  = dx*dx + dy*dy + dz*dz;
+				if (dist > 5*5*5) {
+					delete this.loadedblocks[keys[i]];
+				}
+			}
+			
+			this.find("tiledmap").datasource = this;		
+		}
+		
 		this.updateLoadQueue = function(){
-			console.log('.');
-			if (this.requestPending) return;
+			
+			if (this.currentRequest) return; // already loading something...
+			
 			if (this.loadqueue.length > 0){
 				var zscalar = 1;
+				
 				// sort queue on distance to cursor
 				for (var i = 0;i<this.loadqueue.length;i++){
 					var q = this.loadqueue[i];
@@ -35,31 +94,83 @@ define.class('$server/composition', function vectormap(require,  $server$, filei
 					var dz = (this.centerz - q.z)*zscalar;
 					q.dist = dx * dx + dy * dy + dz * dz;
 				}
-				this.loadqueue.sort(function(a,b){if (a.dist < b.dist) return -1; if (a.dist > b.dist) return 1; return 0;});
 				
-				var first = this.loadqueue.shift();
-				this.requestPending = true;
+				this.loadqueue = this.loadqueue.sort(function(a,b){
+					if (a.dist > b.dist) return -1;
+					if (a.dist < b.dist) return 1; 
+					return 0;
+				});
+				
+				this.currentRequest = this.loadqueue.pop(); 
+				// take closest one from the queue
+				// this.requestPending = true;
 			}
 		}
 		
-		this.init = function(){
-			console.log(" init");
-			this.setInterval(function(){this.updateLoadQueue();}.bind(this), 50);
-
+		this.destroy = function(){
+			this.clearInterval(this.theinterval);
 		}
-
+		
+		this.init = function(){
+			console.log(this.geo.tile_size);
+			this.theinterval = this.setInterval(function(){
+				this.updateLoadQueue();
+			}.bind(this), 50);
+			
+			this.loadinterval = this.setInterval(function(){
+				this.simulateLoaded();
+			}.bind(this), 50);
+		}
 	})
 	
 	define.class(this, "tiledmap", function($ui$, view)
-	{
+	{		
+		this.render = function(){
+			//console.log("rendering!");
+			var res = [];
+			var keys = Object.keys(this.datasource.loadedblocks);
+			for(var i =0 ;i<keys.length;i++){
+				var lb = this.datasource.loadedblocks[keys[i]]
+			//	console.log(lb.x, lb.y);
+				res.push(view({width:18, height:18, position:"absolute", x: 100 + 20*lb.x, y:100 + 20*lb.y,bgcolor:vec4(Math.sin(lb.z), Math.cos(lb.z), 1, 1)}))
+			}
+			return res;
+		}
+		
 		this.attributes = {
-			centerx: Config({value:0}),
-			centery: Config({value:0}),
+			centerx:   Config({value:0}),
+			centery:   Config({value:0}),
 			zoomlevel: Config({value:4, motion:"inoutquad", duration:1.7}),
 			levels: [],
-			blocksize: 150				
+			blocksize: 150,
+			dataset: {}				
 		}
+		
+		
+		this.oncenterx = function(){
+			if (this.dataset){
+				
+				this.datasource.setCenter(Math.floor(this.centerx),Math.floor(this.centery),Math.floor(this.zoomlevel));		
+		
+			}
+		}
+		this.oncentery = function(){
+			if (this.dataset){
+				
+				this.datasource.setCenter(Math.floor(this.centerx),Math.floor(this.centery),Math.floor(this.zoomlevel));		
+		
+			}
+		}
+	
 
+		this.keydownLeftarrow = function(){
+			this.centerx --;
+		}
+		
+		this.keydownRightarrow = function(){
+			this.centerx ++;
+		}
+		
 		this.keydown = function(v){	
 			this.screen.defaultKeyboardHandler(this, v);					
 		}
@@ -74,8 +185,8 @@ define.class('$server/composition', function vectormap(require,  $server$, filei
 			this.color_blend = 'src_alpha * src_color + dst_color'
   
 			this.vertexstruct =  define.struct({		
-					pos:vec3,
-					id: float
+				pos:vec3,
+				id: float
 			})
 
 			this.mesh = this.vertexstruct.array(10000);
@@ -138,7 +249,9 @@ define.class('$server/composition', function vectormap(require,  $server$, filei
 		this.flex = 1;
 		this.overflow = "hidden" 
 		
-		this.moveTo = function(x,y,z){		
+		this.moveTo = function(x,y,z){	
+
+			//this.datasource.setCenter(x,y,z);		
 			var dist = vec2((x?x:this.centerx) - this.centerx,(y?y:this.centery) - this.centery);
 			var l = vec2.len(dist);
 			var totaltime = Math.max(1.,Math.min(10, l*0.3));
@@ -168,12 +281,6 @@ define.class('$server/composition', function vectormap(require,  $server$, filei
 				this.zoomlevel = Animate(zanim);		
 			}
 
-			for(var xx = -3; xx < 3; xx++){
-				for(var yy = -3; yy < 3; yy++){			
-					// add square around 
-					this.addToQueue(x + xx,y + yy,z);	
-				}
-			}
 		}
 		
 	
@@ -181,12 +288,23 @@ define.class('$server/composition', function vectormap(require,  $server$, filei
 			
 			for(var i = 0;i<18;i++){
 				this.levels[i] = {}
-			}
-			
-		}
-		
+			}			
+		}		
 	})
 	
+	this.renderbuttongrid = function(z){
+				res = [];
+				
+				for (var y = 0;y<10;y+=2){
+					var R = [];
+				for (var x = 0;x<10;x+=2){
+					var t = x + " " + y
+					R.push(button({xtarget:x, ytarget:y, ztarget:z, text:t,click:function(){this.rpc.index.moveTo(this.xtarget,this.ytarget,this.ztarget);}, margin:2}))
+				}
+					res.push(view({bg:0},R));
+				}
+				return res;
+			}
 	this.render = function(){
 		
 		return [
@@ -194,9 +312,9 @@ define.class('$server/composition', function vectormap(require,  $server$, filei
 			screen({name:"index", clearcolor:vec4("#000030"), moveTo:function(x,y,z){
 				this.find("tiledmap").moveTo(x,y,z)
 			}}		
-				,this.mapdataset()
-				,this.tiledmap({name:"tiledmap" })
-				
+				,mapdata =this.mapdataset()
+
+				,this.tiledmap({name:"tiledmap", datasource: mapdata})				
 			) 
 			
 		,screen({
@@ -205,21 +323,19 @@ define.class('$server/composition', function vectormap(require,  $server$, filei
 			init:function(){
 				console.log(this.rpc.index)
 			},
+			
 			clearcolor:vec4('darkgray'), overflow:'hidden', title:"VectorMap remote" },
 			speakergrid({justifycontent:"center", alignitems:"center" }, view({width:300, bg:0, flexdirection:"column", alignself:"center"}
 				,label({fontsize:40, text:"Zoompan" , bg:0})
-				,button({text:"0,0,5",click:function(){this.rpc.index.moveTo(0,0,5);}, margin:2})
-				,button({text:"1,0,5",click:function(){this.rpc.index.moveTo(1,0,5);}, margin:2})
-				,button({text:"0,1,5",click:function(){this.rpc.index.moveTo(0,1,5);}, margin:2})
-				,button({text:"0,0,6",click:function(){this.rpc.index.moveTo(0,0,6);}, margin:2})
-				,button({text:"4,0,4",click:function(){this.rpc.index.moveTo(4,0,4);}, margin:2})
-				,button({text:"0,2,4",click:function(){this.rpc.index.moveTo(0,2,4);}, margin:2})
+				,label({fontsize:20, text:"level 5" , bg:0})
+				,view({bg:0},this.renderbuttongrid(5))
+				,label({fontsize:20, text:"level 4" , bg:0})
+				,view({bg:0},this.renderbuttongrid(4))
+				,label({fontsize:20, text:"level 3" , bg:0})
+				,view({bg:0},this.renderbuttongrid(3))
 				,button({text:"Manhattan",click:function(){this.rpc.index.moveTo(9647*2,12320*2, 16);}, margin:2})
 				,button({text:"Amsterdam",click:function(){this.rpc.index.moveTo(33656,21534, 16);}, margin:2})
-				,button({text:"Noord Amsterdam",click:function(){this.rpc.index.moveTo(33656,21434, 16);}, margin:2})
-				,button({text:"Sausalito",click:function(){this.rpc.index.moveTo(9647,12320, 16);}, margin:2})
-				,button({text:"San Fransisco",click:function(){this.rpc.index.moveTo(9647,12320, 16);}, margin:2})
-			))
+					))
 		)
 		]
 	}
