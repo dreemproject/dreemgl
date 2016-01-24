e154268d0e76db135c48108507553f33ad006eff
Max Carlson <max@maxcarlson.com>
2016-01-23 20:14:04 -0800

    Fix example

--- examples/request/index.js
+++ examples/request/index.js
@@ -1,6 +1,6 @@
 define.class('$server/composition', function($ui$, screen, view, label, $$, get) {
     this.render = function() { return [
         get({url:'http://slashdot.org'}),
-        screen({}, label({ text: wire('rpc.get.response'), fgcolor: "black"}))
+        screen({}, label({ text: wire('rpc.get.response'), fgcolor: "black", multiline: true}))
     ] }
 });
