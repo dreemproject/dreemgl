6e0471588698012fc345f186c593d4c082377ac6
Aki Rodic <aleksandar.xyz@gmail.com>
2016-01-27 00:16:03 +0100

    DREEM-104070851: Timeline fix for 60fps thanks to @onejsdev!

--- compositions/timeline/timeline.js
+++ compositions/timeline/timeline.js
@@ -7,16 +7,23 @@ define.class('$ui/view', function (background) {
 	}
 
 	// TODO need a way to externally set attribute without triggering render function.
-	this.oninit = function () {
-		this.animate = function () {
-			requestAnimationFrame(window._animate);
-			this.zoom = 365 / (1 + (0.5 * Math.sin(Date.now() / 5000) * 365 + 365 / 2))
-		}.bind(this);
-		if (!window._animate) {
-			window._animate = this.animate.bind(this);
-			window._animate();
-		}
-		window._animate = this.animate.bind(this);
+	// this.oninit = function () {
+	// 	this.animate = function () {
+	// 		requestAnimationFrame(window._animate);
+	// 		this.zoom = 365 / (1 + (0.5 * Math.sin(Date.now() / 5000) * 365 + 365 / 2))
+	// 	}.bind(this);
+	// 	if (!window._animate) {
+	// 		window._animate = this.animate.bind(this);
+	// 		window._animate();
+	// 	}
+	// 	window._animate = this.animate.bind(this);
+	// }
+
+	this.atDraw = function(){
+		this.zoom = 365 / (1 + (0.5 * Math.sin(Date.now() / 5000) * 365 + 365 / 2))
+		this.setTimeout(function(){
+			this.redraw()
+		}.bind(this), 0)
 	}
 
 	this.render = function() {
