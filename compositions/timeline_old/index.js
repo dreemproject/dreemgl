bda900d975e0b1e1d656fc052d552667adc0601e
Aki Rodic <aleksandar.xyz@gmail.com>
2016-01-28 17:46:10 +0100

    Removed old clunky calendar/timeline prototypes

--- compositions/timeline_old/index.js
+++
@@ -1,9 +0,0 @@
-define.class('$server/composition', function(require, timeline, $ui$, screen, view) {
-	this.render = function() {
-		return [
-			screen({name:'index'},
-				timeline()
-			)
-		]
-	}
-})
