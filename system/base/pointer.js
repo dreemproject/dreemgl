764a3619f14d6c6d38ca43ec35411a0a6adaf1d5
Aki Rodic <aleksandar.xyz@gmail.com>
2016-02-03 23:29:57 +0100

    Fixed pointermove movement

--- system/base/pointer.js
+++ system/base/pointer.js
@@ -113,6 +113,7 @@ define.class('$system/base/node', function(){
 		this.min = pointer.min
 		this.max = pointer.max
 		this.dt = pointer.dt
+		this.movement = pointer.movement
 		this.t = Date.now()
 		if (pointer.wheel !== undefined) this.wheel = pointer.wheel
 	}
