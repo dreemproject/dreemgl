68e7bb4bdab80e161eaa891785316c47143ddb47
gnovos <freemason@gmail.com>
2016-02-08 15:57:04 -0800

    cleanup example

--- examples/toolkit.js
+++ examples/toolkit.js
@@ -25,7 +25,8 @@ define.class("$server/composition",function(require, $ui$, button, icon, label, 
 					view({height:257, width:451, bgcolor:vec4(0.12939296662807465,0.4936515688896179,0.6595012545585632,1), position:'absolute', x:377.924560546875, y:55.101898193359375, rotate:vec3(0,0,10)}),
 					label({fontsize:190, bgcolor:'transparent', fgcolor:vec4(0.9903326034545898,0.8860607147216797,0,1), text:'Howdy!', position:'absolute', x:117.06063842773438, y:106.86102294921875, rotate:vec3(0,0,-0.55)}),
 					icon({fgcolor:vec4(0.3921568691730499,0.929411768913269,0.42688411474227905,1), icon:'anchor', position:'absolute', x:726.0000610351562, y:165, fontsize:180}),
-					icon({fgcolor:'cornflower', icon:'flask', position:'absolute', x:910, y:485.9999694824219})
+					icon({fgcolor:vec4(0.929411768913269,0.9167269468307495,0.3921568691730499,1), icon:'gear', position:'absolute', x:779, y:106, fontsize:480}),
+					icon({fgcolor:'cornflower', icon:'glass', position:'absolute', x:962, y:40.99998474121094, fontsize:180})
 				),
 				toolkit({
 					flexdirection:"column"
