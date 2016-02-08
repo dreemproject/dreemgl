d15c881babddcd21375c75c87ad74c19cca37595
gnovos <freemason@gmail.com>
2016-02-08 01:55:22 -0800

    clean up example

--- examples/toolkit.js
+++ examples/toolkit.js
@@ -21,10 +21,10 @@ define.class("$server/composition",function(require, $ui$, icon, label, view, sc
 						toolmove:false, 
 						toolrect:false
 					},
-					view({height:473, width:618, bgcolor:vec4(0.608367383480072,0.9318798184394836,0.31568998098373413,1), position:'absolute', x:39, y:120.00009155273438}),
-					view({height:257, width:451, bgcolor:vec4(0.26616209745407104,0.7584172487258911,0.5933423042297363,1), position:'absolute', x:667.0001220703125, y:284.0000305175781}),
+					view({height:473, width:618, bgcolor:vec4(0.9318798184394836,0.31568998098373413,0.7469818592071533,1), position:'absolute', x:122, y:131.00009155273438}),
+					view({height:257, width:451, bgcolor:vec4(0.12939296662807465,0.4936515688896179,0.6595012545585632,1), position:'absolute', x:598.0231628417969, y:372.4164123535156, rotate:vec3(0,0,10)}),
 					label({fontsize:190, bgcolor:'transparent', fgcolor:vec4(0.9903326034545898,0.8860607147216797,0,1), text:'Howdy!', position:'absolute', x:185.77291870117188, y:250.35304260253906, rotate:vec3(0,0,-0.55)}),
-					icon({fgcolor:vec4(0.8438690900802612,0.4203698933124542,0.7602147459983826,1), icon:'gear', position:'absolute', x:754.0001220703125, y:47.99999237060547, fontsize:80}),
+					icon({fgcolor:vec4(0.8438690900802612,0.4203698933124542,0.7602147459983826,1), icon:'gear', position:'absolute', x:754.0001831054688, y:47.99999237060547, fontsize:80}),
 					icon({fgcolor:vec4(0.3921568691730499,0.929411768913269,0.42688411474227905,1), icon:'anchor', position:'absolute', x:930.0001220703125, y:37.999969482421875, fontsize:180})
 				),
 				toolkit({flexdirection:"column"})
