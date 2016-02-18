f704fb34d31f2367e5a2b373acea7446a120f45a
onejsdev <onejsdev@gmail.com>
2016-02-18 09:59:33 +0100

    Remove compositions dir

--- apps/flowgraph/flowtest1/index.js
+++ apps/flowgraph/flowtest1/index.js
@@ -1,9 +1,9 @@
 define.class("$server/composition",function($server$, service, $ui$, screen, view, $flow$services$, map, webrequest, $flow$controllers$, xypad, knob, keyboard, dpad, $flow$displays$, labtext) {
 	this.render = function() {
 		return [
-			xypad({name:"xypad0", flowdata:{x:81, y:279}}),
-			labtext({name:"labtext0", flowdata:{x:856, y:216}, vec2:wire("this.rpc.xypad0.pointerpos"), string:wire("this.rpc.dpad0.value")}),
-			dpad({name:"dpad0", flowdata:{x:69, y:49}})
+			xypad({name:"xypad0", flowdata:{x:23, y:316}}),
+			labtext({name:"labtext0", flowdata:{x:290, y:164}, vec2:wire("this.rpc.xypad0.pointerpos"), string:wire("this.rpc.dpad0.value")}),
+			dpad({name:"dpad0", flowdata:{x:71, y:59}})
 		]
 	}
 }
