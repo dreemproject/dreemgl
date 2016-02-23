6c806d73fe4e806e5fce6d15af7bc92b93f070e7
Stijn Kuipers <stijnkuipers@gmail.com>
2016-02-23 16:58:13 +0100

    Openstreetmaps styling

--- classes/widgets/map/mapstyle.js
+++ classes/widgets/map/mapstyle.js
@@ -5,11 +5,11 @@
 
 define.class(function(require){
 
-	var watercolor = vec4("#c0d5d5");
+	var watercolor = vec4("#90cccb");
 
 	this.mapstyle = {
 		ferry:{
-			roadcolor: vec4(0.6784313917160034,0.8470588326454163,0.9019607901573181,1),
+			roadcolor: vec4(0.6784313917160034,0.8470588326454163,0.9019607901573181,1)
 		},
 		riverbank:{color1:watercolor},
 		marina:{color1:watercolor},
@@ -17,7 +17,7 @@ define.class(function(require){
 		stream:{color1:watercolor},
 		canal:{color1:watercolor},
 		lake:{color1:watercolor},
-		playa:{color1:vec4("#f0f0c0")},
+		playa:{color1:watercolor},
 		river:{color1:watercolor},
 		basin:{color1:watercolor},
 		swimming_pool:{color1:watercolor},
@@ -27,118 +27,92 @@ define.class(function(require){
 		ocean:{
 			color1:watercolor
 		},
-		
+
 		earth:{
-			color1: vec4("#c0c0c0"),
-			
+			color1: vec4("#f6f5f0")
 		},
 		national_park:{
-			color1:vec4("d3fad3"),
-			
+			color1:vec4("#b2c29d")
 		},
 		park:{
 			offset:-7,
-			color1: vec4("#bad6ab",1),
-			color2: vec4(0.003921568859368563,0.19607843458652496,0.125490203499794,1),
+			color1: vec4("#d0dcae", 1)
 		},
 		residential:{
-			color1: vec4("#cdc7bf"),
+			color1: vec4("#ffffff")
 		},
 		industrial:{
-			color1:vec4("#202020"),
-			color2:vec4("#d0d0d0")
+			color1:vec4("#dcd6d6")
 		},
 		recreation_ground:{
-			
-			color1:vec4("#8080f0"),
-			color2:vec4("#6060f0")
+			color1:vec4("#deddbe")
 		},
 		scrub:{
-			color1: vec4("#97d398"),		
+			color1: vec4("#e1e8d6")
 		},
 		wetland:{
 			offset:-18.5,
-			color1: vec4("#97b3b8"),
-			color2: vec4("darkblue")
-
+			color1: vec4("#e2e8e1")
 		},
 		beach:{
 			offset:-19,
-			color1: vec4("#f0f0a0"),
-			color2: vec4("yellow")
-
+			color1: vec4("#f0f0e8")
 		},
 		nature_reserve:{
 			offset:-18,
-			color1: vec4("green"),
-			color2: vec4("green")
+			color1: vec4("#deddbe")
 		},
 		commercial:{
 		},
 		golf_course:{
 		},
 		farm:{
-			color1: vec4("#d3fad3"),
-			color2: vec4(1,1,0.10000000149011612,1),
+			color1: vec4("#e6e5e0")
 		},
 		grass:{
-			
-			color1:vec4("#d3fad3"),
+			color1: vec4("#d0dcae")
 		},
 		sports_centre:{
-			color1: vec4(1,0,0,1),
-			color2: vec4(1,1,1,1),
+			color1: vec4(1,0,0,1)
 		},
 		farmland:{
-			color1:vec4("#d3fad3"),
-			
+			color1: vec4("#dddcbd")
 		},
 		hospital:{
 		},
 		retail:{
 			offset:27,
-			color1: vec4(0,0,1,0.5),
-			color2: vec4(0,0,1,0.5),
+			color1: vec4('#dcd6d6')
 		},
 		allotments:{
 		},
 		runway:{
 			offset:-20,
-
-			color1:vec4("#c0c0c0"),
-			color2:vec4("#d0d0d0")
+			color1:vec4("#dcdf9d")
 		},
 		aerodrome:{
 			offset:-20,
-
-			color1:vec4("#c0c0c0"),
-			color2:vec4("#d0d0d0")
+			color1:vec4("#dddddd")
 		},
 		forest:{
 			offset:-30,
-			color1: vec4("#b7d3a8"),
-			
+			color1: vec4("#b2c29d")
 		},
 		meadow:{
-
-			color1: vec4("#b7d3a8"),
+			color1: vec4("#deddbe")
 		},
 		parking:{
 			offset:-8,
-			color1: vec4(0.501960813999176,0.501960813999176,0.501960813999176,1),
-			color2: vec4(0.8274509906768799,0.8274509906768799,0.8274509906768799,1),
+			color1: vec4(0.501960813999176,0.501960813999176,0.501960813999176,1)
 		},
 		plant:{
-			color1:vec4("#90f050"),
-			color2:vec4("#208020")
+			color1: vec4("#b2c29d")
 		},
 		pitch:{
-			color1: vec4("#d3fad3"),
-			
+			color1: vec4("#efeecd")
 		},
 		cemetery:{
-			color1: vec4("gray"),
-			color2: vec4("darkgray")
+			color1: vec4("gray")
 		},
 		zoo:{
 		},
@@ -146,8 +120,7 @@ define.class(function(require){
 		},
 		university:{
 			offset:-21,
-			color1: vec4(1,0,0,1),
-			color2: vec4(0,0,0,1),
+			color1: vec4('#dcd6d6')
 		},
 		apron:{
 		},
@@ -157,34 +130,31 @@ define.class(function(require){
 		},
 		playground:{
 			offset:-19,
-			color1: vec4("lightblue"),
-			color2: vec4(1,0,0,1),
+			color1: vec4("#f0eee4")
 		},
 		stadium:{
 		},
 		railway:{
-			roadcolor: vec4("#5e5e5e"),
-			color1: vec4("#5e5e5e"),
+			roadcolor: vec4("#f6f5f0"),
+			color1: vec4("#f6f5f0"),
 			sortkey:3
 		},
 		rail:{
-			roadcolor: vec4("#5e5e5e"),
+			roadcolor: vec4("#999999"),
 			sortkey:3
 		},
 		garden:{
-			color1: vec4("#90f050"),
+			color1: vec4("#d0dcae")
 		},
 		farmyard:{
-			color1:vec4("#808010"),
-			color2:vec4("#606020")
+			color1: vec4("#deddbe")
 		},
 		generator:{
 		},
 		college:{
 		},
 		pedestrian:{
-			color1: vec4(0.8274509906768799,0.8274509906768799,0.8274509906768799,1),
-			color2: vec4(1,1,0,1),
+			color1: vec4('#dddde8')
 		},
 		school:{
 		},
@@ -193,74 +163,65 @@ define.class(function(require){
 		petting_zoo:{
 		},
 		wood:{
-			color1:vec4("#90f050"),
-
+			color1:vec4("#b2c19c")
 		},
 		common:{
-			color1: vec4(1,1,1,1),
+			color1: vec4('#f6f5f0')
 		},
 		village_green:{
-			color1: vec4("#90f050"),
+			color1: vec4("#deddbe")
 		},
 		prison:{
 		},
 		racetrack:{
-			roadcolor: vec4("white"),
+			roadcolor: vec4("#eeece2")
 		},
 		major_road:{
-			roadcolor: vec4("#f8faca"),
+			roadcolor: vec4("#d1928d")
 		},
 		Ferry:{
-			roadcolor:vec4("blue"),
+			roadcolor: vec4("#509b9a"),
 			sortkey: -1
 		},
 		highway:{
-			roadcolor: vec4("#f4c0ab"),
-
-			sortkey:2,
+			roadcolor: vec4("#a098b0"),
+			sortkey:2
 			},
 		minor_road:{
-			roadcolor: vec4("white"),
+			roadcolor: vec4("#dcdf9d")
 		},
-				Road:{
-			roadcolor: vec4("#f4c0ab")
+		Road:{
+			roadcolor: vec4("#d2938e")
 		},
-
 		undefined:{
 		},
 		works:{
 		},
 		protected_area:{
-			color1: vec4("red"),
-			color2: vec4("green")
+			color1: vec4("#f6f5f0")
 		},
 		theme_park:{
 		},
 		path:{
-			roadcolor: vec4(0.8156862854957581,0.8156862854957581,0.8156862854957581,1),
+			roadcolor: vec4('#ebe9dc')
 		},
 		quarry:{
-			color1: vec4("gray"),
-			color2: vec4("black")
+			color1: vec4("#cccccc")
 		},
 		bridge:{
 			offset:-22,
-			color1: vec4(0.501960813999176,0.501960813999176,0.501960813999176,1),
-			color2: vec4(1,1,1,1),
+			color1: vec4('#e8cab0')
 		},
 		breakwater:{
 			offset:-20,
-			color1: watercolor,
-			color2: 0,
+			color1: watercolor
 		},
 		water:{
 			offset:0,
-			color1: watercolor,
-			color2: 0,
+			color1: watercolor
 		},
 		building:{
-			color1:vec4("#cac0b6"),
-			color2:vec4("white")
+			color1: vec4("#dcd6d6")
 		},
 		apartments:{
 		},
@@ -270,8 +231,7 @@ define.class(function(require){
 		},
 		pier:{
 			offset:-5,
-			color1: vec4(0.250980406999588,0.250980406999588,0.250980406999588,1),
-			color2: vec4(0.501960813999176,0.501960813999176,0.501960813999176,1),
+			color1: vec4('#f6f5f0')
 		},
 		place_of_worship:{
 		},
@@ -285,8 +245,7 @@ define.class(function(require){
 		},
 		footway:{
 			offset:-12,
-			color1: vec4(0.501960813999176,0.501960813999176,0.501960813999176,1),
-			color2: vec4(1,1,0,1),
+			color1: vec4('#eeeeee')
 		},
 		groyne:{
 		},
@@ -294,8 +253,7 @@ define.class(function(require){
 		},
 		default:
 		{
-			color1:vec4('red'),
-			color2:vec4('blue')
+			color1: vec4('#fafafc')
 		}
 	}
 })
