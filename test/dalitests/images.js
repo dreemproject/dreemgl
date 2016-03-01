e60f980374afd928a2caa17d4182fed66ec44329
Raju Bitter <rajubitter@gmail.com>
2016-03-01 20:06:38 +0100

    Split up Flexbox guide into two parts to avoid running out of WebGL contexts in Chrome (max of 8 per page).

--- test/dalitests/images.js
+++ test/dalitests/images.js
@@ -13,7 +13,8 @@ define.class(function($server$, composition, $ui$, screen, view, label, require)
 			for (var w=100; w<=200; w+= 25) {
 				var v1 = view({
 					size: vec2(w, w)
-					,bgimage: require('./assets/' + digit + '.png')
+					,bgimage: define.classPath(this) + 'assets/' + digit + '.png'
+					,bgimagemode:"stretch"
 				})
 				dynviews.push(v1);
 			}
