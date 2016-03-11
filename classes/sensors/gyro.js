3605e2d10d219572a084bde00a077848c53967d1
gnovos <freemason@gmail.com>
2016-03-10 21:36:44 -0800

    add gyro example

--- classes/sensors/gyro.js
+++ classes/sensors/gyro.js
@@ -4,11 +4,80 @@
  software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and limitations under the License.*/
 
-define.class("$ui/view", function(require) {
+define.class("$system/base/node", function(require) {
 // 	The gyro receives gyroscope and compass data where available.
 // See [https://w3c.github.io/deviceorientation/spec-source-orientation.html#deviceorientation](https://w3c.github.io/deviceorientation/spec-source-orientation.html#deviceorientation)
 // and [https://developer.apple.com/library/safari/documentation/SafariDOMAdditions/Reference/DeviceOrientationEventClassRef/DeviceOrientationEvent/DeviceOrientationEvent.html](https://developer.apple.com/library/safari/documentation/SafariDOMAdditions/Reference/DeviceOrientationEventClassRef/DeviceOrientationEvent/DeviceOrientationEvent.html) for details.
 
+	this.attributes = {
+		// True if gyro is supported
+		supported:Config({type:Boolean, value:false}),
+
+		acceleration:Config({type:vec3, value:vec3(0,0,0)}),
+
+		// alias for the x component of acceleration
+		x: Config({alias:'acceleration', index:0}),
+
+		// alias for the y component of acceleration
+		y: Config({alias:'acceleration', index:1}),
+
+		// alias for the z component of acceleration
+		z: Config({alias:'acceleration', index:2}),
+
+		orientation:Config({type:vec3, value:vec3(0,0,0)}),
+
+		// alias for the alpha component of orientation
+		alpha: Config({alias:'orientation', index:0}),
+
+		// alias for the beta component of orientation
+		beta: Config({alias:'orientation', index:1}),
+
+		// alias for the gamma component of orientation
+		gamma: Config({alias:'orientation', index:2}),
+
+		// The compass orientation, see [https://developer.apple.com/library/safari/documentation/SafariDOMAdditions/Reference/DeviceOrientationEventClassRef/DeviceOrientationEvent/DeviceOrientationEvent.html](https://developer.apple.com/library/safari/documentation/SafariDOMAdditions/Reference/DeviceOrientationEventClassRef/DeviceOrientationEvent/DeviceOrientationEvent.html) for details.
+		compass:Config({type:Number, value:0}),
+
+		// The compass accuracy, see [https://developer.apple.com/library/safari/documentation/SafariDOMAdditions/Reference/DeviceOrientationEventClassRef/DeviceOrientationEvent/DeviceOrientationEvent.html](https://developer.apple.com/library/safari/documentation/SafariDOMAdditions/Reference/DeviceOrientationEventClassRef/DeviceOrientationEvent/DeviceOrientationEvent.html) for details.
+		accuracy:Config({type:Number, value:0})
+	};
+
+	this.init = function() {
+		// TODO: use lzidle to throttle
+		window.addEventListener('devicemotion', function(ev) {
+			var accel = ev.accelerationIncludingGravity
+			if (accel) {
+				for (var key in accel) {
+					if (accel.hasOwnProperty(key)) {
+						var value = ev.accelerationIncludingGravity[key]
+						if (typeof(value) !== "undefined") {
+							if (!this._supported) {
+								this.supported = true;
+							}
+							this[key] = value
+						}
+					}
+				}
+			}
+		}.bind(this));
+
+		window.addEventListener('deviceorientation', function(ev) {
+			var orientationevents = ['alpha', 'beta', 'gamma'];
+			for (var i = 0; i < orientationevents.length;i++) {
+				var name = orientationevents[i];
+				var value = ev[name];
+				if (typeof(value) !== "undefined") {
+					if (!this._supported) {
+						this.supported = true;
+					}
+					this[name] = value;
+				}
+			}
+
+			this.accuracy = ev.webkitCompassAccuracy;
+			this.compass = ev.webkitCompassHeading;
+		}.bind(this));
+	};
 
 	var gyro = this.constructor;
 	this.constructor.examples = {
@@ -18,4 +87,4 @@ define.class("$ui/view", function(require) {
 			]
 		}
 	}
-})
+});
--- examples/sensors.js
+++ examples/sensors.js
@@ -9,11 +9,45 @@ define.class('$server/composition', function($ui$, screen, cadgrid, view, icon, 
 	this.render = function() {
 		return [
 			screen({name:'default', clearcolor:vec4('black')},
+				gyro({
+					name:"gyro",
+					onacceleration:function(ev,v,o) {
+						o.find("x").text = v[0];
+						o.find("y").text = v[1];
+						o.find("z").text = v[2];
+					},
+					onorientation:function(ev,v,o) {
+						o.find("alpha").text = v[0];
+						o.find("beta").text  = v[1];
+						o.find("gamma").text = v[2];
+					},
+					onsupported:function(ev,v,o) {
+						o.find("searching").visible = false;
+						o.find("gyrout").visible = true;
+					}
+				}),
 				cadgrid({
 						flex:1,
-						justifycontent:"center",
+						flexdirection:"column",
+						justifycontent:"space-around",
 						alignitems:"center"
-					}
+					},
+					label({name:"searching", visible:true, text:"Searching for gyro device...", fgcolor:"#666", fontsize:40}),
+
+					view({  name:"gyrout",
+						    visible:false,
+							flexdirection:"column",
+							justifycontent:"space-around",
+							alignitems:"center"
+						},
+						label({marginbottom:15, text:"Move your device to see\ngyroscope values change:", fgcolor:"#666", fontsize:20}),
+						view({padding:5}, label({marginright:10, fgcolor:"red", text:"x"}),     label({fgcolor:"blue", name:"x", text:"0"}) ),
+						view({padding:5}, label({marginright:10, fgcolor:"red", text:"y"}),     label({fgcolor:"blue", name:"y", text:"0"}) ),
+						view({padding:5}, label({marginright:10, fgcolor:"red", text:"z"}),     label({fgcolor:"blue", name:"z", text:"0"}) ),
+						view({padding:5}, label({marginright:10, fgcolor:"red", text:"alpha"}), label({fgcolor:"blue", name:"alpha", text:"0"}) ),
+						view({padding:5}, label({marginright:10, fgcolor:"red", text:"beta"}),  label({fgcolor:"blue", name:"beta", text:"0"}) ),
+						view({padding:5}, label({marginright:10, fgcolor:"red", text:"gamma"}), label({fgcolor:"blue", name:"gamma", text:"0"}) )
+					)
 				)
 			)
 		]
