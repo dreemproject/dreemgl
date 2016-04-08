3580dd7fe780c4ada6ca5d199362158b72b2ed1f
gnovos <freemason@gmail.com>
2016-04-08 16:50:41 -0700

    don't need such a mesy example

--- test/multiwire.js
+++ test/multiwire.js
@@ -4,7 +4,7 @@ define.class("$server/composition",function($server$,service,$ui$,screen,$flow$d
     return [
       outputs({name:"a",flowdata:{x:26,y:37,screen:true}}),
       outputs({name:"b",flowdata:{x:17,y:589,screen:true}}),
-      debug({name:'default',clearcolor:'#484230',flowdata:{x:939,y:244,screen:true},array:wire("[this.rpc.a.number,this.rpc.a.float,this.rpc.a.vec2,this.rpc.a.vec4,this.rpc.a.string,this.rpc.b.object,this.rpc.b.array,this.rpc.b.vec3,this.rpc.b.int,this.rpc.b.boolean]"),object:wire("{\"a.int\":this.rpc.a.int,\"a.boolean\":this.rpc.a.boolean,\"a.vec3\":this.rpc.a.vec3,\"a.array\":this.rpc.a.array,\"a.object\":this.rpc.a.object,\"b.string\":this.rpc.b.string,\"b.vec4\":this.rpc.b.vec4,\"b.vec2\":this.rpc.b.vec2,\"b.float\":this.rpc.b.float,\"b.number\":this.rpc.b.number}"),number:wire("this.rpc.a.number"),boolean:wire("this.rpc.b.boolean"),float:wire("this.rpc.a.float"),int:wire("this.rpc.b.int"),vec2:wire("this.rpc.a.vec2"),vec3:wire("this.rpc.b.vec3"),vec4:wire("this.rpc.a.vec4"),string:wire("this.rpc.b.string")})
+      debug({name:'default',clearcolor:'#484230',flowdata:{x:939,y:244,screen:true},array:wire("[this.rpc.a.number,this.rpc.b.number]"),object:wire("{\"a.int\":this.rpc.a.int,\"b.number\":this.rpc.b.number}"),string:wire("this.rpc.b.string")})
     ]
   }
   
