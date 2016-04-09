bc746f61a4908d6d87ae6a2bab267362b1859e8d
gnovos <freemason@gmail.com>
2016-04-09 13:48:06 -0700

    cleanup multiwire

--- test/multiwire.js
+++ test/multiwire.js
@@ -3,8 +3,9 @@ define.class("$server/composition",function($server$,service,$ui$,screen,$flow$d
   this.render=function(){
     return [
       outputs({name:"a",flowdata:{x:26,y:37,screen:true}}),
-      outputs({name:"b",flowdata:{x:17,y:589,screen:true}}),
-      debug({name:'default',clearcolor:'#484230',flowdata:{x:511,y:243,screen:true},object:wire("{"a.int":this.rpc.a.int}")})
+      outputs({name:"b",flowdata:{x:17,y:481,screen:true}}),
+      debug({name:'default',clearcolor:'#484230',flowdata:{x:484,y:159,screen:true},array:wire("[this.rpc.a.number,this.rpc.a.vec2,this.rpc.b.vec4]"),object:wire("{\"b.object\":this.rpc.b.object,\"a.object\":this.rpc.a.object}")}),
+      debug({name:"debug0",flowdata:{x:496,y:500,screen:true},object:wire("this.rpc.b.object"),array:wire("[this.rpc.b.number]"),number:wire("this.rpc.b.number")})
     ]
   }
   
