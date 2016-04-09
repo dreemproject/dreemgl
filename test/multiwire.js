c4589e7e6124a0cd4ab2bd59faf3377c06d19a30
gnovos <freemason@gmail.com>
2016-04-09 14:15:27 -0700

    fix bug

--- test/multiwire.js
+++ test/multiwire.js
@@ -4,8 +4,8 @@ define.class("$server/composition",function($server$,service,$ui$,screen,$flow$d
     return [
       outputs({name:"a",flowdata:{x:26,y:37,screen:true}}),
       outputs({name:"b",flowdata:{x:17,y:481,screen:true}}),
-      debug({name:'default',clearcolor:'#484230',flowdata:{x:484,y:159,screen:true},array:wire("[this.rpc.a.number,this.rpc.b.vec4,this.rpc.a.vec2,this.rpc.a.vec4]"),object:wire("{\"b.object\":this.rpc.b.object,\"a.object\":this.rpc.a.object}"),number:wire("this.rpc.a.int")}),
-      debug({name:"debug0",flowdata:{x:496,y:500,screen:true},object:wire("this.rpc.b.object"),array:wire("[this.rpc.b.number]"),number:wire("this.rpc.b.number")})
+      debug({name:'default',clearcolor:'#484230',flowdata:{x:484,y:159,screen:true},array:wire("[this.rpc.a.number,this.rpc.a.vec2,this.rpc.b.vec4,this.rpc.a.int,this.rpc.b.object]"),object:wire("{\"a.array\":this.rpc.a.array,\"b.array\":this.rpc.b.array}")}),
+      debug({name:"debug0",flowdata:{x:496,y:500,screen:true},array:wire("[this.rpc.b.object,this.rpc.a.object,this.rpc.b.array]"),object:wire("this.rpc.b.object"),float:wire("this.rpc.b.number"),number:wire("this.rpc.b.int")})
     ]
   }
   
