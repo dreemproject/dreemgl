a0bd8dd0bfcb1fa377114925ec8e875526a8fd36
promanik <philip@pbrdev.com>
2016-04-19 09:01:58 -0400

    Improved Gaussian blur. The display only shows the results for one of the two passes.

--- classes/ui/blurview.js
+++ classes/ui/blurview.js
@@ -9,9 +9,10 @@
 define.class('$ui/view', function(require){
 // Example of how to use multiple render passes
 
+	// k0 is the weight for the center pixel. k1 is used for +1 and -1 away,
+	// k2 is used for +2 and -2 away, ...
 	this.attributes = {
-
-		//TODO
+		k0: Config({type: float, value:1.0}),
 		k1: Config({type: float, value:1.0}),
 		k2: Config({type: float, value:1.0}),
 		k3: Config({type: float, value:1.0}),
@@ -22,55 +23,81 @@ define.class('$ui/view', function(require){
 		k8: Config({type: float, value:1.0}),
 		k9: Config({type: float, value:1.0}),
 		k10: Config({type: float, value:1.0}),
-		k11: Config({type: float, value:1.0}),
-		k12: Config({type: float, value:1.0}),
-		k13: Config({type: float, value:1.0}),
-		k14: Config({type: float, value:1.0}),
-		k15: Config({type: float, value:1.0}),
-		k16: Config({type: float, value:1.0}),
-		k17: Config({type: float, value:1.0}),
 		filterlength: Config({type: int, value:3}),
 		filterscaling: Config({type: float, value:1.0}),
 
 		blurradius: 2.
 	}
 
+	// Compute the kernel parameters given blurradius.
+	// The value of sigma is .5 * blurradius
+	// The filter length is 2*blurradius + 1 (in the range of 1 to 21)
+	// For large blurradius, 21 filter parameters are computed.
+	this.buildKernel = function() {
+		var sigma = 0.5 * this.blurradius;
+
+		var radius = Math.floor(this.blurradius);
+		if (radius < 1) radius = 1;
+		if (radius > 10) radius = 10;
+
+		// Compute the kernel as an array. Compute one side only, but keep track
+		// of the sum from both sides.
+		var kernel = [];
+    var scaling = 1.0 / (Math.sqrt(2 * Math.PI) * sigma);
+		var sum = 0.0;
+		for (var x=0; x<radius+1; x++) {
+			var g = Math.exp(-x*x/(2 * sigma * sigma)) * scaling;
+			sum += g;
+			if (x > 0) sum += g;
+			kernel.push(g);
+		}
+
+		// Normalize the kernel so that the terms sum to 1
+		for (var i=0; i<kernel.length; i++) {
+			kernel[i] = kernel[i] / sum;
+		}
+
+		//console.log('kernel', radius, kernel);
+
+		// Set the k values (shaders don't support arrays)
+		this.filterscaling = 1.0;
+		this.filterlength = 2 * radius + 1;
+		this.k0 = kernel[0];
+		this.k1 = kernel[1];
+		this.k2 = kernel[2];
+		this.k3 = kernel[3];
+		this.k4 = kernel[4];
+		this.k5 = kernel[5];
+		this.k6 = kernel[6];
+		this.k7 = kernel[7];
+		this.k8 = kernel[8];
+		this.k9 = kernel[9];
+		this.k10 = kernel[10];
+	}
+
+
+	this.atConstructor = function() {
+		this.buildKernel();
+	}
+
+	// Rebuild the kernel when the blurradius changes
+	this.onblurradius = function(ev,v,o) {
+		this.buildKernel();
+	}
+
 	// Must define N RenderPass nested classes below to match this count
-	this.passes = 8
+	this.passes = 2
 	// Required for multipass to work
 	this.overflow = 'hidden'
 
 
-	// x or y filter for a 3x3 1 sigma Gaussian
-	define.class(this, "ConvPass3", this.RenderPass, function() {
-		this.k1 = 0.25
-		this.k2 = 0.50
-		this.k3 = 0.25
-		this.scale = 1.0
-		this.spacing = vec2(0.001, 0)
+	// x or y shader filter for a 1D Gaussian
+	define.class(this, "Gaussian1D", this.RenderPass, function() {
+		// Shader requires all instance variables to be defined.
+		this.spacing = vec2(1, 0)
 
 		this.color = function() {
-			var col = this.framebuffer.conv1d3(mesh.xy, this.k1, this.k2, this.k3, this.scale, this.spacing)
-
-			return vec4(col.rgb, col.a * view.opacity)
-		}
-	})
-
-
-	// x or y filter for a 7x7 1 sigma Gaussian
-	define.class(this, "ConvPass7", this.RenderPass, function() {
-		this.k1 = 0.006
-		this.k2 = 0.061
-		this.k3 = 0.242
-		this.k4 = 0.383
-		this.k5 = 0.242
-		this.k6 = 0.061
-		this.k7 = 0.006
-		this.scale = 1.0
-		this.spacing = vec2(0.001, 0)
-
-		this.color = function() {
-			var col = this.framebuffer.conv1d7(mesh.xy, this.k1, this.k2, this.k3, this.k4, this.k5, this.k6, this.k7, this.scale, this.spacing)
+			var col = this.framebuffer.conv1d(mesh.xy, view.filterlength, view.filterscaling, this.spacing, view.k0, view.k1, view.k1, view.k2, view.k2, view.k3, view.k3, view.k4, view.k4, view.k5, view.k5, view.k6, view.k6, view.k7, view.k7, view.k8, view.k8, view.k9, view.k9, view.k10, view.k10)
 
 			return vec4(col.rgb, col.a * view.opacity)
 		}
@@ -79,27 +106,24 @@ define.class('$ui/view', function(require){
 
 	// Each pass _must_ be named pass0..9, define based on this.passes, e.g. this.passes of
 	// 1 must define pass0, 2 must define pass0 and pass1...
-	define.class(this, "pass0", this.ConvPass7, function() {
+	define.class(this, "pass0", this.Gaussian1D, function() {
 		this.atConstructor = function(view) {
 			// horizontal filter
-			var width = view._layout.width ? view._layout.width : view.screen.size.x;
-			this.spacing = vec2(1.0 / width, 0)
+			//var width = view._layout.width ? view._layout.width : view.screen.size.x;
+			//this.spacing = vec2(1.0 / width, 0)
+
+			this.spacing = vec2(1.0, 0)
 		}
 	})
 
-	define.class(this, "pass1", this.ConvPass7, function() {
+	define.class(this, "pass1", this.Gaussian1D, function() {
 		this.atConstructor = function(view) {
 			// vertical filter
-			var height = view._layout.height ? view._layout.height : view.screen.size.y;
-			this.spacing = vec2(0, 1.0 / height)
+			//var height = view._layout.height ? view._layout.height : view.screen.size.y;
+			//this.spacing = vec2(0, 1.0 / height)
+
+			this.spacing = vec2(0, 1.0)
 		}
 	})
-
-	define.class(this, "pass2", this.pass0, function() {})
-	define.class(this, "pass3", this.pass1, function() {})
-	define.class(this, "pass4", this.pass0, function() {})
-	define.class(this, "pass5", this.pass1, function() {})
-	define.class(this, "pass6", this.pass0, function() {})
-	define.class(this, "pass7", this.pass1, function() {})
 
 })
--- test/blurviewtest.js
+++ test/blurviewtest.js
@@ -4,23 +4,44 @@
    software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and limitations under the License.*/
 
-define.class(function($server$, composition, $ui$, screen, view, blurview, icon, label, require){
+define.class(function($server$, composition, $ui$, screen, view, blurview, icon, label, slider, require){
 	this.render = function(){
 		return [
 			screen({name:'default'},
-				 blurview({flex: 1, blurradius: 3.0},
-					icon({icon: 'chain', fontsize: 100}),
-					label({text: 'Hello!', fontsize: 100}),
-					view({
-							width:200,
-							height:200,
-							borderwidth:1,
-							bordercolor:"white",
-							bgimagemode:"center",
-							bgimage:require('$resources/textures/landscape.jpg')
-					})
-				)
-			)
+						 view({flex: 1, flexdirection:"column"},
+									slider({
+										flex:0,
+										width:300,
+										minhandlethreshold:26,
+										height:15,
+										value:0.1,
+										bgcolor:"darkyellow",
+										fgcolor:"white",
+										onvalue:function(ev,v,o) {
+											// Scale the blurradius from 0-25
+											var blur = 1 + v * 25;
+											
+											var view = this.find('blur');
+											if (view) {
+												view.blurradius = blur;
+											}
+										}
+									}),
+
+									blurview({name: 'blur', flex: 1, blurradius: 8},
+													 icon({icon: 'chain', fontsize: 100}),
+													 label({text: 'Hello!', fontsize: 100}),
+													 view({
+														 width:200,
+														 height:200,
+														 borderwidth:1,
+														 bordercolor:"white",
+														 bgimagemode:"center",
+														 bgimage:require('$resources/textures/landscape.jpg')
+													 })
+													)
+								 )
+						)
 		]
 	}
 })
